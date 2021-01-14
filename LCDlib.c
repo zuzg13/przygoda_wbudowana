87b6bdb455ad891f5d2dac0425597ef837b95c99
zuzg13 <nanaxoxo33@gmail.com>
2021-01-14 18:05:33 +0100

    Add draw line function

--- LCDlib.c
+++ LCDlib.c
@@ -45,3 +45,88 @@
 					lcdWriteData(LCD_RegValue);
 		}
 	}
+	
+	
+ void drawLine(const int x1, const int y1, const int x2, const int y2, uint16_t LCD_RegValue)
+ {
+     // zmienne pomocnicze
+     int d, dx, dy, ai, bi, xi, yi;
+     int x = x1, y = y1;
+     // ustalenie kierunku rysowania
+     if (x1 < x2)
+     {
+         xi = 1;
+         dx = x2 - x1;
+     }
+     else
+     {
+         xi = -1;
+         dx = x1 - x2;
+     }
+     // ustalenie kierunku rysowania
+     if (y1 < y2)
+     {
+         yi = 1;
+         dy = y2 - y1;
+     }
+     else
+     {
+         yi = -1;
+         dy = y1 - y2;
+     }
+     // pierwszy piksel
+     lcdSetCursor(x, y);
+		 lcdWriteReg(DATA_RAM, LCD_RegValue);
+		 
+     // os wiodaca OX
+     if (dx > dy)
+     {
+         ai = (dy - dx) * 2;
+         bi = dy * 2;
+         d = bi - dx;
+         // petla po kolejnych x
+         while (x != x2)
+         {
+             // test wspólczynnika
+             if (d >= 0)
+             {
+                 x += xi;
+                 y += yi;
+                 d += ai;
+             }
+             else
+             {
+                 d += bi;
+                 x += xi;
+             }
+							lcdSetCursor(x, y);
+							lcdWriteReg(DATA_RAM, LCD_RegValue);
+         }
+     }
+     // os wiodaca OY
+     else
+     {
+         ai = ( dx - dy ) * 2;
+         bi = dx * 2;
+         d = bi - dy;
+         // petla po kolejnych y
+         while (y != y2)
+         {
+             // test wspólczynnika
+             if (d >= 0)
+             {
+                 x += xi;
+                 y += yi;
+                 d += ai;
+             }
+             else
+             {
+                 d += bi;
+                 y += yi;
+             }
+             lcdSetCursor(x, y);
+						 lcdWriteReg(DATA_RAM, LCD_RegValue);
+         }
+     }
+ }
+		
--- LCDlib.h
+++ LCDlib.h
@@ -8,3 +8,5 @@
 void printChar(char znaczek, int x, int y, uint16_t LCD_RegValue);
 void printString(char* str, int x, int y,  uint16_t LCD_RegValue);
 void setBackground(uint16_t LCD_RegValue);
+void drawLine(const int x1, const int y1, const int x2, const int y2, uint16_t LCD_RegValue);
+
