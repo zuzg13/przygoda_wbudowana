3887368d06f2b332dc7842edea77828b15088c6f
Marta Grzesiak <mrtgrzesiak@gmail.com>
2021-01-22 12:52:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,10 +7,10 @@
 #include "Timerlib.h"
 #include "DHT11Communication.h"
 #include "PIN_LPC17xx.h"
-#include "Board_Buttons.h" // ::Board Support:Buttons
+#include "Board_Buttons.h"
 #include "IAP.h"
 
-
+//macros created based on LPC1768 user manual
 #define SECTOR 29
 #define START 0x00078000
 #define END 0x0007FFFF
@@ -38,12 +38,12 @@ int main(void)
 	/* initalization of average temperature and humidity values */
 	float average_minute_temp = 0.;
 	float average_ten_temp = 0.;
-    float average_minute_hum = 0.;
-    float average_ten_hum=0.;
+    	float average_minute_hum = 0.;
+    	float average_ten_hum=0.;
 	
 	/* initalization of counters */
 	int counter1 = 0;
-    int counter2 = 0;
+    	int counter2 = 0;
 
 	char xc[13];
 	char xc2[13];
@@ -107,6 +107,7 @@ int main(void)
 		{
 			if(LPC_TIM0->TC > 40) break; //Timeout
 		}
+		
 		unsigned int time = stopTimer0();
 		
 		if(time < 10 || time > 40) 
@@ -148,57 +149,57 @@ int main(void)
 		
 		/* calculating average values and writing it to flash memory */
 		if(LPC_TIM1->TC > 60000000) // if one minute lasts, new values are calculated
-        {
+        	{
 		    stopTimer1();
 				
 		    average_ten_temp += average_minute_temp/(float)(counter1);
-            average_ten_hum += average_minute_hum/(float)(counter1);
+            	    average_ten_hum += average_minute_hum/(float)(counter1);
 			
-            if(counter2 > 10)
-            {
-                counter2 = 0;
-                average_ten_temp = 0;
-                average_ten_hum = 0;
-            }
-            else
-            {
-                counter2++;
+            	    if(counter2 > 10)
+            	    {
+                 	counter2 = 0;
+                	average_ten_temp = 0;
+                	average_ten_hum = 0;
+            	    }
+            	    else
+             	    {
+                	counter2++;
             
-				if(counter2 == 1) // new average values are written to flash memory only once in every 10 minutes - for the firtst time
-				{
-					Chip_IAP_PreSectorForReadWrite(SECTOR, SECTOR);
-					Chip_IAP_EraseSector(SECTOR, SECTOR);
-					Chip_IAP_PreSectorForReadWrite(SECTOR, SECTOR);
+			if(counter2 == 1) // new average values are written to flash memory only once in every 10 minutes - for the firtst time
+			{
+				Chip_IAP_PreSectorForReadWrite(SECTOR, SECTOR);
+				Chip_IAP_EraseSector(SECTOR, SECTOR);
+				Chip_IAP_PreSectorForReadWrite(SECTOR, SECTOR);
 								
-					/* guarantee rewriting of values*/
-					data_from_past[5] = data_from_past[3];
-					data_from_past[4] = data_from_past[2];
-					data_from_past[3] = data_from_past[1];
-					data_from_past[2] = data_from_past[0];
+				/* guarantee rewriting of values*/
+				data_from_past[5] = data_from_past[3];
+				data_from_past[4] = data_from_past[2];
+				data_from_past[3] = data_from_past[1];
+				data_from_past[2] = data_from_past[0];
 							
-					data_from_past[0] = average_minute_temp/(float)(counter1);
-					data_from_past[1] = average_minute_hum/(float)(counter1);
+				data_from_past[0] = average_minute_temp/(float)(counter1);
+				data_from_past[1] = average_minute_hum/(float)(counter1);
 						
-					Chip_IAP_CopyRamToFlash(START, data_from_past, 512);
+				Chip_IAP_CopyRamToFlash(START, data_from_past, 512);
 
-				}
+			}
 						
-				average_minute_temp = 0.;
-				average_minute_hum = 0.;
-				counter1 = 0;
-            }
+			average_minute_temp = 0.;
+			average_minute_hum = 0.;
+			counter1 = 0;
+            	    }
 				
 		    startTimer1();
-        }
+        	}
 		else
-        {
-				/* add new values to sum*/
-				if((int)dataBytes[0]<200 && (int)dataBytes[2] < 100)
-				{
-					average_minute_temp += dataBytes[0]; 
-                    average_minute_hum += dataBytes[2];
-					counter1++;
-				}			
+        	{
+			/* add new values to sum*/
+			if((int)dataBytes[0]<200 && (int)dataBytes[2] < 100)
+			{
+				average_minute_temp += dataBytes[0]; 
+                    		average_minute_hum += dataBytes[2];
+				counter1++;
+			}			
 		}
 
 		
@@ -221,20 +222,19 @@ int main(void)
 			}
 			
 			printString("Wartosci z przeszlosci: ", 110, 15, LCDBlue);
-					uint32_t* tmp = (uint32_t*) START;
-					sprintf(xc3, "%dC %d %%", *tmp, *(tmp+1));
-					printString(xc3, 90, 15, LCDBlue);
-					sprintf(xc3, "%dC %d %%", *(tmp+2), *(tmp+3));
-					printString(xc3, 75, 15, LCDBlue);
-					sprintf(xc3, "%dC %d %%", *(tmp+4), *(tmp+5));
-					printString(xc3, 60, 15, LCDBlue);
+			uint32_t* tmp = (uint32_t*) START;
+			sprintf(xc3, "%dC %d %%", *tmp, *(tmp+1));
+			printString(xc3, 90, 15, LCDBlue);
+			sprintf(xc3, "%dC %d %%", *(tmp+2), *(tmp+3));
+			printString(xc3, 75, 15, LCDBlue);
+			sprintf(xc3, "%dC %d %%", *(tmp+4), *(tmp+5));
+			printString(xc3, 60, 15, LCDBlue);
 			
 		}
 		else //if state == 1 print plots
 		{
 			printString(xc, 230, 160, LCDWhite);
 				
-			
 			int temp_y = 10*pos_x+15;
 			int temp_x = ((int)(dataBytes[0])-start_y)* 10 + 5; 
 			int hum_x = ((int)(dataBytes[2])-start_y)* 10 + 5; 
@@ -247,8 +247,8 @@ int main(void)
 
 			if(pos_x<31)
 			{
-                pos_x++;
-            }
+                		pos_x++;
+            		}
 			else
 			{
 				pos_x=1;
@@ -257,7 +257,7 @@ int main(void)
 				drawLine(5,5,5,310,LCDRed);
 
 				for(int i = 1; i <= 22; i++)
-	                drawLine(i*10+5,1,i*10+5,10,LCDRed);
+	                		drawLine(i*10+5,1,i*10+5,10,LCDRed);
 
 			}
 
